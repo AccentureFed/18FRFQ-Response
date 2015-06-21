5670ed8b0fe13aa070ad96a93c468bef7569b2c7
sean.kosanovich <sean.kosanovich@agilex.com>
2015-06-21 19:47:59 -0400

    Refactoring and unit tests for normalization servifes

--- jigsaw/grails-app/controllers/com/afs/food/recall/FoodRecallController.groovy
+++ jigsaw/grails-app/controllers/com/afs/food/recall/FoodRecallController.groovy
@@ -1,13 +1,10 @@
 package com.afs.food.recall
 
-
-
-
 class FoodRecallController {
 
     def foodRecallService
 
     def recalls() {
-        render foodRecallService.getAllRecalls()
+        render foodRecallService.getRecalls()
     }
 }
--- jigsaw/grails-app/services/com/afs/food/recall/FoodRecallService.groovy
+++ jigsaw/grails-app/services/com/afs/food/recall/FoodRecallService.groovy
@@ -4,56 +4,34 @@ import grails.transaction.Transactional
 
 import org.codehaus.groovy.grails.web.json.JSONObject
 
-import com.afs.jigsaw.fda.food.api.State
-import com.google.common.collect.Lists
-
 @Transactional
 class FoodRecallService {
 
+    /**
+     * The maximum amount of results to return from the FDA Food service.<br /><br />
+     *
+     * Note: The API will not support returning more than 100, do not set above 100
+     */
     private static final def MAX_RESULTS = 100
     private static final def BASE_URL = "https://api.fda.gov/food/enforcement.json?limit=${MAX_RESULTS}"
 
-    private static final def MULTI_WORDED_STATES = []
-    static {
-        State.values().each { state ->
-            if(state.getFullName().contains(' ')) {
-                MULTI_WORDED_STATES << state
-            }
-        }
-    }
+    def stateNormalizationService
 
-    def getAllRecalls() {
+    /**
+     * Returns the first {@link #MAX_RESULTS} recalls from the FDA Service API.  The data is passed back as a {@link JSONObject}.<br /><br />
+     *
+     * Each return will have an added JSON Array field called 'normalized_distribution_pattern' that will contain all of the state codes each recall was distributed in.
+     * @return A {@link JSONObject} representing the recalls.
+     */
+    def getRecalls() {
         def json = new JSONObject(new URL("${BASE_URL}").getText())
-        def totalResults = json.meta.results.total
 
         final Set<String> distributionStates = []
         json.results.each { result ->
-            distributionStates.clear()
-
-            def pattern = result.distribution_pattern
-            if(pattern.contains('all 50 U.S. States') || pattern.contains('nationwide')) {
-                // add all 50 states and bail out
-                distributionStates.addAll(State.values()*.getAbbreviation())
-            } else {
-
-                MULTI_WORDED_STATES.each { multiWordSate ->
-                    multiWordSate.getAllFullNames().each {  fullName ->
-                        if(pattern.contains(fullName)) {
-                            distributionStates << multiWordSate.getAbbreviation()
-                            pattern = pattern.replace(fullName, '')
-                        }
-                    }
-                }
-
-                pattern.tokenize().each { distribution_item ->
-                    def state = State.fromString(distribution_item.replace(',', '').replace('.',''))
-                    if(state) {
-                        // found a state
-                        distributionStates << state.getAbbreviation()
-                    }
-                }
-            }
-            result.normalized_distribution_pattern = Lists.newArrayList(distributionStates)
+            // try to find the states in the natural language value and add it to the result
+            result.normalized_distribution_pattern = stateNormalizationService.getStates(result.distribution_pattern)*.getAbbreviation()
         }
+
+        return json
     }
 }
