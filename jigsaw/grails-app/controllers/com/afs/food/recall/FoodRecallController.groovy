42f66184962ee6fe4fcb6f90ec089549227ae2ca
Greg Roberts <greg.roberts@agilex.com>
2015-06-22 17:32:27 -0400

    beginnings of notification service

--- jigsaw/grails-app/controllers/com/afs/food/recall/FoodRecallController.groovy
+++ jigsaw/grails-app/controllers/com/afs/food/recall/FoodRecallController.groovy
@@ -7,4 +7,12 @@ class FoodRecallController {
     def recalls() {
         render foodRecallService.getRecalls() 
     }
+	
+	def readRSS() {
+		render foodRecallService.readRss()
+	}
+	
+	def sendNotifications() {
+		render foodRecallService.sendNotifications()
+	}
 }
--- jigsaw/grails-app/services/com/afs/food/recall/FoodRecallService.groovy
+++ jigsaw/grails-app/services/com/afs/food/recall/FoodRecallService.groovy
@@ -1,6 +1,9 @@
 package com.afs.food.recall
 
 import grails.transaction.Transactional
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 import org.codehaus.groovy.grails.web.json.JSONObject
 
@@ -16,6 +19,7 @@ class FoodRecallService {
     private static final def BASE_URL = "https://api.fda.gov/food/enforcement.json?limit=${MAX_RESULTS}"
 
     def stateNormalizationService
+	LocalDate lastNotified = LocalDate.now().minusMonths(2)
 
     /**
      * Returns the first {@link #MAX_RESULTS} recalls from the FDA Service API.  The data is passed back as a {@link JSONObject}.<br /><br />
@@ -34,4 +38,32 @@ class FoodRecallService {
 
         return json
     }
+	
+	def needUpdate() {
+		def json = new JSONObject(new URL("https://api.fda.gov/food/enforcement.json").getText())
+		LocalDate lastUpdate = LocalDate.parse(json.meta.last_updated, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+		return (lastUpdate > lastNotified)
+	}
+	
+	def sendNotifications(){
+		println("Last notified: ${lastNotified}")
+		if(needUpdate()){
+			println("Sending notifications")
+			def json = new JSONObject(new URL("https://api.fda.gov/food/enforcement.json?search=recall_initiation_date:[${lastNotified}+TO+${LocalDate.now()}]&limit=100").getText())
+			final Set<String> distributionStates = []
+			json.results.each { result ->
+				println("${result.recall_number} - ${result.recall_initiation_date}")
+			}
+			lastNotified = LocalDate.parse(json.meta.last_updated, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+		}
+	}
+	
+	def readRss(){
+		def url = "http://www2c.cdc.gov/podcasts/createrss.asp?c=146"
+		def rss = new XmlSlurper().parse(url)
+		println rss.channel.title
+		rss.channel.item.each { item->
+			println "- ${item.title}" 
+		}​
+	}
 }
