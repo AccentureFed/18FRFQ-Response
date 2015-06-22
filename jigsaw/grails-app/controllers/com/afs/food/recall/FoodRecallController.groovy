b702901847503c2b729f6a1475034bf0c4351af4
peterdailey <peterdailey@10.1.68.57>
2015-06-22 14:16:41 -0400

    Adding count API by state

--- jigsaw/grails-app/controllers/com/afs/food/recall/FoodRecallController.groovy
+++ jigsaw/grails-app/controllers/com/afs/food/recall/FoodRecallController.groovy
@@ -7,4 +7,11 @@ class FoodRecallController {
     def recalls() {
         render foodRecallService.getRecalls()
     }
+
+	def count() {
+		
+		render foodRecallService.getCountsByState(com.afs.jigsaw.fda.food.api.State.fromString(params.stateCode));
+		
+	}
+
 }
--- jigsaw/grails-app/services/com/afs/food/recall/FoodRecallService.groovy
+++ jigsaw/grails-app/services/com/afs/food/recall/FoodRecallService.groovy
@@ -4,7 +4,9 @@ import grails.transaction.Transactional
 
 import org.codehaus.groovy.grails.web.json.JSONObject
 
-@Transactional
+import com.afs.jigsaw.fda.food.api.StateSearchCriteriaUtils
+
+
 class FoodRecallService {
 
     /**
@@ -14,9 +16,22 @@ class FoodRecallService {
      */
     private static final def MAX_RESULTS = 100
     private static final def BASE_URL = "https://api.fda.gov/food/enforcement.json?limit=${MAX_RESULTS}"
-
+	protected static final def BASE_COUNT_URL = "https://api.fda.gov/food/enforcement.json?"
+	
     def stateNormalizationService
 
+	
+	/*
+	 *  this creates are "nlp phrases" list
+	 *  states with multi words will be treated as a single phrase
+	 */
+	private static final Map CLASSIFICATION_TO_SEVERITY= new HashMap<String,String>();
+	static {
+	CLASSIFICATION_TO_SEVERITY.put("Class I", "high");
+	CLASSIFICATION_TO_SEVERITY.put("Class II", "medium");
+	CLASSIFICATION_TO_SEVERITY.put("Class III", "low");
+	}
+	
     /**
      * Returns the first {@link #MAX_RESULTS} recalls from the FDA Service API.  The data is passed back as a {@link JSONObject}.<br /><br />
      *
@@ -34,4 +49,65 @@ class FoodRecallService {
 
         return json
     }
+
+
+	/**
+	 * Returns the aggregated counts by classification for a specific state.  The data is passed back in a JSON
+	 * {@link JSONObject}.<br /><br />
+	 * 
+	 * There is no query feature in the FDA API to support state by state querying of the data set. It is possible to acheive this 
+	 * same behavior through specially structured search criteria which will use AND/OR syntax to include the state code or state
+	 * name.  In cases where the state name is an exact substring of another state (i.e. Virginia / West Virginia), the AND
+	 * syntax will be used to use Virginia AND NOT West Virginia. <br /><br /> 
+	 * 
+	 * The resulting JSON will have the "classification"  of Class I, Class II, Class III
+	 *  removed in place of "severity" of high medium and low instead. <br /><br />
+	 * 
+	 *   
+	 * @return
+	 */
+	def getCountsByState(com.afs.jigsaw.fda.food.api.State state) {
+		//these functions are separated out to facilitate unit testng code coverage
+		def json = new JSONObject(new URL(buildCountUrl(state)).getText())
+		return transformCountJson(state, json);
+		
+
+	}	
+	
+	/**
+	 * Uses utilities available
+	 * @param state
+	 * @return
+	 */
+	def buildCountUrl (com.afs.jigsaw.fda.food.api.State state) {
+		String searchCriteria = new StateSearchCriteriaUtils().generateCriteria(state);
+		StringBuffer url = new StringBuffer();
+		url.append(BASE_COUNT_URL).append("search=").append(searchCriteria).append("&count=classification.exact");
+		
+		return url.toString();
+	}
+	
+	/**
+	 * Transforms the JSON return from the FDA API to the spec
+	 * @param json
+	 * @return
+	 */
+	def transformCountJson(com.afs.jigsaw.fda.food.api.State state, JSONObject json) {
+		def translatedJson = new JSONObject();
+		translatedJson.stateCode = state.getAbbreviation()
+		
+		json.results.each { result ->
+			// try to find the states in the natural language value and add it to the result
+			def entry= new JSONObject();
+			if (CLASSIFICATION_TO_SEVERITY.containsKey(result.term)) {
+				/**
+				 * error handling for a changing set of values in the underlying API...
+				 */
+				entry.severity = CLASSIFICATION_TO_SEVERITY.getAt(result.term);
+				entry.count = result.count
+				translatedJson.accumulate("results", entry);
+			}
+		}
+		return translatedJson
+	}
 }
