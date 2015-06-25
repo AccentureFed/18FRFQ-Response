a3753e46065005fbd77698e9ca7509f1b5a124a8
bakerj <joshua.baker@agilex.com>
2015-06-25 09:31:44 -0400

    removed nginfinite-scroll

--- jigsaw/grails-app/assets/javascripts/app/app.js
+++ jigsaw/grails-app/assets/javascripts/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('jigsawApp', ['LocalStorageModule', 'tmh.dynamicLocale',
-    'ngResource', 'ui.router', 'ngCookies', 'ngCacheBuster', 'infinite-scroll', 'angular-ladda'])
+    'ngResource', 'ui.router', 'ngCookies', 'ngCacheBuster', 'angular-ladda'])
 
     .run(function ($rootScope, $location, $window, $http, $state, ENV, VERSION) {
         $rootScope.ENV = ENV;
